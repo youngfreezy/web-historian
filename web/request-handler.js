cfb04b08268eeed3dc54b860f1cbcb29ee8457ec
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-09 14:18:05 -0700

    passing 2 tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var fs = require('fs');
+var http = require('http');
+var getter = require('http-get')
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
-};
+var handleRequest = function (req, res) {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, '../web/public/index.html'), function(err, data){
+      if(err){
+        throw err;
+      }
+      res.writeHeader(200, helpers.headers);
+      res.end(data)
+    })
+  } else {
+    res.writeHeader(404);
+    res.end('404, file not found')
+  }
+  
+}
+
+exports.handleRequest = handleRequest;
+//exports.getPage = getPage;
